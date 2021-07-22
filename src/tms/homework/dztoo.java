75769c3696abf701d56bd19acb8dee3d8ab094bc
Denis V <denis13071981@gmail.com>
2021-07-22 19:56:59 +0300

    Initial commit

--- src/tms/homework/dztoo.java
+++ src/tms/homework/dztoo.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class dztoo {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
+        System.out.println("Введите число с тройкой в конце:");
         Scanner inData = new Scanner(System.in);
         int getData = inData.nextInt();
-        System.out.println(getData % 10 != 3 ? "Ваше число не заканчивается 3" : "Ваше число заканчивается 3");
+        int val = getData % 10;
+        System.out.println(val != 3 ? "Ваше число заканчивается на " + val : "То что нужно )))");
     }
 }
