26fea5faba1ee137ffce028fd2b32a116fceed4a
Denis V <denis13071981@gmail.com>
2021-07-22 21:53:04 +0300

    Initial commit

---
+++ src/tms/homework/dzfive.java
@@ -0,0 +1,24 @@
+package tms.homework;
+
+import java.util.Scanner;
+
+public class dzfive {
+    public static void main(String[] args) {
+        System.out.println("Введите какую-нибудь сумму:");
+        Scanner inData = new Scanner(System.in);
+        int getCost = inData.nextInt();
+        String cost = "Спасибо, с вашего счета будет списано: ";
+        cost = cost.concat(getCost + " ");
+        boolean validate = false;
+        if (getCost % 10 == 1) {
+            cost = cost.concat("рубль.");
+        } else if (getCost % 10 == 0 || (getCost % 10 > 4 && getCost % 10 < 9)) {
+            cost = cost.concat("рублей.");
+        } else if (getCost % 10 > 1 && getCost % 10 < 5) {
+            cost = cost.concat("рубля.");
+        } else {
+            System.out.println("Что-то пошло не так");   //на всякий случай
+        }
+        System.out.println(cost);
+    }
+}
