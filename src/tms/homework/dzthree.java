e1522bcfeebc5e7846148b81cb6ba1e013baa604
Denis V <denis13071981@gmail.com>
2021-07-22 20:08:36 +0300

    Initial commit

--- src/tms/homework/dzthree.java
+++ src/tms/homework/dzthree.java
@@ -1,7 +1,14 @@
 package tms.homework;
 
+import java.util.Scanner;
+
 public class dzthree {
     public static void main(String[] args) {
-
+        System.out.println("Введите размер первой стороны прямоугольика:");
+        Scanner inData = new Scanner(System.in);
+        int getA = inData.nextInt();
+        System.out.println("Введите размер второй стороны прямоугольика:");
+        int getB = inData.nextInt();
+        System.out.println("Площадь = " + getA * getB + " ,а периметр = " + (getA + getB) * 2);
     }
 }
