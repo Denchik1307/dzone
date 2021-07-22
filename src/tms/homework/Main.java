e50a43ae5386bbef6cce6ca330e1613622025e9b
Denis V <denis13071981@gmail.com>
2021-07-22 19:40:40 +0300

    Initial commit

---
+++ src/tms/homework/Main.java
@@ -0,0 +1,14 @@
+package tms.homework;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Введите число:");
+        Scanner inData = new Scanner(System.in);
+        int getData = inData.nextInt();
+        System.out.println("Ваше число " + getData + " в кубе = " + Math.pow(getData, 3));
+    }
+}
