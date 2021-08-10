4936606f162904ae3cf1b434bc16850db8949ec4
Denis V <denis13071981@gmail.com>
2021-08-10 22:47:06 +0300

    Initial commit

--- src/tms/homeworkfour/Main.java
+++ src/tms/homeworkfour/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         CheckPalindromString text = new CheckPalindromString("");
         System.out.println(text.palindromBuilder("D321 1 2 3d"));
+
+        //System.out.println(RomeToArabNumber("XMD"));
+        RomeToArabNumber numberArab = new RomeToArabNumber();
+        System.out.println(numberArab.romeToArabNumber("mix"));
     }
 }
--- src/tms/homeworkfour/RomeToArabNumber.java
+++ src/tms/homeworkfour/RomeToArabNumber.java
@@ -1,14 +1,62 @@
 package tms.homeworkfour;
 
-public class RomeToArabNumber {
+public final class RomeToArabNumber {
+    private int val = 0;
+    private int val_next = 0;
+    private int temp = 0;
+    private int result = 0;
 
-    public enum RomeToInt {
-        I(1), V(5), X(10), L(50), C(100), D(500), M(1000);
-        private final int value;
+    public int romeToArabNumber(String roman){
+        roman = roman.toUpperCase();
+        for (int i = 0; i < roman.length(); i++) {
+            val = converter(roman.charAt(i));
+            if (i < roman.length() - 1) {
+                val_next = converter(roman.charAt(i + 1));
+            } else val_next = 0;
 
-        private RomeToInt(int value) {
-            this.value = value;
+
+            if (val_next == 0) {
+                temp = val;
+            } else {
+                if (val > val_next) temp = val;
+                else if (val < val_next) {
+                    temp = val_next - val;
+                    i++;
+                } else {
+                    temp = val;
+                }
+            }
+            result += temp;
         }
+        return result;
+    }
 
+    private int converter(char val) {
+        int a = 0;
+        switch (val) {
+            case 'M':
+                a = 1000;
+                break;
+            case 'D':
+                a = 500;
+                break;
+            case 'C':
+                a = 100;
+                break;
+            case 'L':
+                a = 50;
+                break;
+            case 'X':
+                a = 10;
+                break;
+            case 'V':
+                a = 5;
+                break;
+            case 'I':
+                a = 1;
+                break;
+            default:
+        }
+        return a;
     }
 }
